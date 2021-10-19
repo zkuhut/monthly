6528d0f274733b3be14a1ddb1f69e24e0c962b7e
lencx <cxin1314@gmail.com>
2021-10-20 01:53:04 +0800

    chore: add guide

---
+++ posts/.vitepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Monthly',
+  description: '',
+  base: '/',
+
+  themeConfig: {
+		docsDir: 'posts',
+    repo: 'zkuorg/monthly',
+    repoLabel: '点 ⭐️ 不迷路',
+    docsBranch: 'main',
+    editLinks: true,
+    editLinkText: 'Edit this page',
+    lastUpdated: 'Last Updated',
+  }
+}
--- posts/index.md
+++ posts/index.md
@@ -1,4 +1,9 @@
-# 贡献人列表
-
-- [lencx](./lencx/about/)
-- [xiaofuyesnew](./xiaofuyesnew/about/index.md)
+---
+home: true
+# heroImage: /logo.png
+heroText: Monthly
+tagline: 输出产生价值
+actionText: 开始阅读
+actionLink: /guide/
+footer: CC0 1.0 Licensed | Copyright © 2021-present @zkuorg
+---
