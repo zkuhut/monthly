41f839034df451284b41250a3bf2f3ea2577854b
lencx <cxin1314@gmail.com>
2021-10-23 12:18:12 +0800

    chore: add scripts

---
+++ scripts/sidebar.ts
@@ -0,0 +1,29 @@
+import fg from 'fast-glob';
+import path from 'path';
+import chalk from 'chalk';
+
+import { vitepressRoot, writeJSON } from './utils';
+
+function sidebarJSON() {
+  const files = fg.sync('**/toc.ts', { cwd: path.resolve('posts') });
+  const sidebars = Object.create({});
+  files.forEach(i => Object.assign(sidebars, require(`../posts/${i}`).default));
+  writeJSON(path.resolve(vitepressRoot, 'sidebar.json'), sidebars);
+  console.log(chalk.green`[TOC]`);
+  const routes = Object.keys(sidebars);
+  routes.forEach((i, idx) => (
+    console.log(chalk.green`  ${idx === routes.length - 1 ? '`' : '|'}- `, chalk.yellow(i), chalk.grey`posts${i}toc.ts`)
+  ));
+  console.log();
+}
+
+function cli() {
+  try {
+    sidebarJSON();
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+if (require.main === module) cli();
