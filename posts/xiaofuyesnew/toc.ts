c9fa1ee849e2876a021969ee51dfb16e1b8a15a0
xiaofuyesnew <xiaofuyesnew@qq.com>
2021-11-25 12:20:08 +0800

    feat(article): basic skills

--- posts/xiaofuyesnew/toc.ts
+++ posts/xiaofuyesnew/toc.ts
@@ -2,5 +2,6 @@ export default {
   '/xiaofuyesnew/': [
     { text: 'About', link: '/xiaofuyesnew/about/' },
     // other post links
+    { text: 'Basic Skills', link: '/xiaofuyesnew/BasicSkills/' }
   ],
 } as TOC;
