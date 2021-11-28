6b6e92fead0d7ec2ef92cc8264fe5ec1ed5766f0
刘超 <liuchao@aldt.com.cn>
2021-11-28 15:36:49 +0800

    feat(): add redux and redux-toolkit article

---
+++ posts/promiseLC/toc.ts
@@ -0,0 +1,7 @@
+export default {
+  '/promiseLC/': [
+    { text: 'About', link: '/promiseLC/about/' },
+    { text: 'redux and redux-toolkit', link: '/promiseLC/redux-toolkit/' },
+    // other post links
+  ],
+} as TOC;
