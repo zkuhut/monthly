8b59918159f22cdc764106dc7e601b933676a0b0
alphaz <alphaz>
2021-12-03 09:51:44 +0800

    feat: release an article named `get_substring_on_bash`.

---
+++ posts/alphaz/toc.ts
@@ -0,0 +1,7 @@
+export default {
+  '/alphaz/': [
+    { text: 'About', link: '/alphaz/about/' },
+    { text: 'Get substring on bash', link: '/alphaz/get_substring_on_bash/'},
+    // other post links
+  ],
+} as TOC;
