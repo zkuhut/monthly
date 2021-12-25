0cc13c23f65fc56ea9b81474b842bb68879e1b0c
xiaofuyesnew <xiaofuyesnew@qq.com>
2021-12-25 15:06:00 +0800

    rebase

--- posts/xiaofuyesnew/toc.ts
+++ posts/xiaofuyesnew/toc.ts
@@ -3,7 +3,12 @@ export default {
     { text: 'About', link: '/xiaofuyesnew/about/' },
     // other post links
     // { text: 'Basic Skills', link: '/xiaofuyesnew/BasicSkills/' }
+<<<<<<< HEAD
     { text: '从零开始搭建一个vite+vue3项目', link: '/xiaofuyesnew/handleVite/' }
 
+=======
+    { text: '从零开始搭建一个vite+vue3项目', link: '/xiaofuyesnew/handleVite/' },
+    { text: '', link: '/xiaofuyesnew//' },
+>>>>>>> 1b96bdb (update)
   ],
 } as TOC;
