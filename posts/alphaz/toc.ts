06162019b00ee73c190207c66bfebec4e5ad87df
alphaz <alphaz>
2022-01-24 12:26:33 +0800

    feat: release an article named .

--- posts/alphaz/toc.ts
+++ posts/alphaz/toc.ts
@@ -2,6 +2,7 @@ export default {
   '/alphaz/': [
     { text: 'About', link: '/alphaz/about/' },
     { text: 'Get substring on bash', link: '/alphaz/get_substring_on_bash/'},
+    { text: 'Analyse nic traffic', link: '/alphaz/analyse_nic_traffic/'},
     // other post links
   ],
 } as TOC;
