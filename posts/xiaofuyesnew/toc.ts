534e2ab22e07476a0c184a55a40c15d180c559b7
xiaofuyesnew <xiaofuyesnew@qq.com>
2022-01-25 18:25:51 +0800

    update

--- posts/xiaofuyesnew/toc.ts
+++ posts/xiaofuyesnew/toc.ts
@@ -5,5 +5,13 @@ export default {
     // { text: 'Basic Skills', link: '/xiaofuyesnew/BasicSkills/' }
     { text: "从零开始搭建一个vite+vue3项目", link: "/xiaofuyesnew/handleVite/" },
     { text: "React 图片预加载组件的实现", link: "/xiaofuyesnew/imagePreload/" },
+    {
+      text: "利用腾讯云开发实现web端的小程序扫码授权",
+      link: "/xiaofuyesnew/miniAuth/",
+      children: [{
+        text: "原理篇",
+        link: "/xiaofuyesnew/miniAuth/intro/",
+      }],
+    },
   ],
 } as TOC;
