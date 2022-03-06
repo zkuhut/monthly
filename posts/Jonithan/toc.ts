0485f71a7d69b3d800b469e7188f13055ceeba32
lihanzhou <lihanzhou@tencent.com>
2022-03-06 22:45:21 +0800

    Jonithan: add post -- useDockerDeployBlog

--- posts/Jonithan/toc.ts
+++ posts/Jonithan/toc.ts
@@ -1,7 +1,8 @@
 export default {
   '/Jonithan/': [
     { text: 'About', link: '/Jonithan/about/' },
-    { text:'React单测之Jest与Karma技术栈对比', link:'/Jonithan/ReactUnitTest-Jest-vs-Karma/'}
+    { text:'React单测之Jest与Karma技术栈对比', link:'/Jonithan/ReactUnitTest-Jest-vs-Karma/'},
+    { text:'使用docker部署博客小记', link:'/Jonithan/useDockerDeployBlog/'}
     // other post links
   ],
 } as TOC;
