86391ed1e1b219ad933b524aaa984e627032c4c9
juvenile <1050299953@qq.com>
2022-04-18 14:46:53 +0800

    business oriented add

--- posts/juvenile/toc.ts
+++ posts/juvenile/toc.ts
@@ -6,6 +6,7 @@ export default {
     { text: 'Code-specification', link: '/juvenile/Code-specification/' },
     { text: 'Code-specification-about', link: '/juvenile/Code-specification/about' },
     { text: 'share', link: '/juvenile/share/' },
+    { text: 'business-oriented', link: '/juvenile/business-oriented/' },
     // other post links
   ],
 } as TOC;
